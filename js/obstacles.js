b7c271472bd64ec95370fa49ffe4afd4019fd6b0
Gabriela Gallango <ga.gallango@gmail.com>
2020-04-08 10:36:33 +0200

    add js/obstacles.js to game

--- js/obstacles.js
+++ js/obstacles.js
@@ -43,3 +43,41 @@
 
 //     }
 // }
+
+
+// BEBIDAS TO CATCH 
+
+// class Mojitos {
+
+//     constructor(ctx, posX, posY, width, height, canvasSize) {
+//         this.ctx = ctx
+//         this.mojito = undefined
+//         this.posX = posX
+//         this.posY = posY
+//         this.width = width
+//         this.height = height
+//         this.canvasSize = {
+//             width: canvasSize.width,
+//             height: canvasSize.height
+//         }
+//     }
+//     draw() {
+
+//         this.mojito = new Image()
+//         this.mojito.src = './img/GAMEArtboard 10@100x.png'
+//         this.mojito.onload = () => this.ctx.drawImage(this.mojito, this.posX, this.posY, this.width, this.height)
+
+//         // this.ctx.fillStyle = 'orange'
+//         // this.ctx.fillRect(this.posX, this.posY, this.width, this.height)
+//     }
+//     // isCollisionMojito(item1, item2) {
+//     //     return (
+//     //         item1.posX < item2.posX + item2.width &&
+//     //         item1.posX + item1.width > item2.posX &&
+//     //         item1.posY < item2.posY + item2.height &&
+//     //         item1.posY + item1.height > item2.posY
+//     //     )
+//     // }
+
+
+}
